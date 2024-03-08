23d591a050fbe5c28a780c4d8df045bb22b3d449
Adriel <adrielranz@gmail.com>
2024-03-08 17:39:09 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/practica_7/Practica_7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica_7;
+
+/**
+ *
+ * @author adrie
+ */
+public class Practica_7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
